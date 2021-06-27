25152e3b80e4ebfcc31ef69675c747f24e77faae
k <mol.tired@gmail.com>
2021-06-27 00:30:54 -0500

    lexer: add token type tests

--- test/lexer.lua
+++ test/lexer.lua
@@ -29,56 +29,135 @@ typedef struct __sToken {
 } Token;
 
 Lex * lex_make(void);
-void lex_read(const char *);
+void lex_readfrom(const char *);
 Token *lex_next(void);
 
 ]]
 
-local tests = {
-	{input = "0", want = "0"},
-	{input = "1", want = "1"},
-	{input = "2", want = "2"},
-	{input = "3", want = "3"},
-	{input = "4", want = "4"},
-	{input = "5", want = "5"},
-	{input = "6", want = "6"},
-	{input = "7", want = "7"},
-	{input = "8", want = "8"},
-	{input = "9", want = "9"},
-	{input = "  0  ", want = "0"},
-	{input = "  1  ", want = "1"},
-	{input = "  2  ", want = "2"},
-	{input = "  3  ", want = "3"},
-	{input = "  4  ", want = "4"},
-	{input = "  5  ", want = "5"},
-	{input = "  6  ", want = "6"},
-	{input = "  7  ", want = "7"},
-	{input = "  8  ", want = "8"},
-	{input = "  9  ", want = "9"},
-	{input = "1234567890", want = "1234567890"},
-	{input = "  1234567890  ", want = "1234567890"},
-	{input = "<", want = "<"},
-	{input = ">", want = ">"},
-	{input = "<&", want = "<&"},
-	{input = ">&", want = ">&"},
-	{input = "<<", want = "<<"},
-	{input = ">>", want = ">>"},
-	{input = "  <  ", want = "<"},
-	{input = "  >  ", want = ">"},
-	{input = "  <&  ", want = "<&"},
-	{input = "  >&  ", want = ">&"},
-	{input = "  <<  ", want = "<<"},
-	{input = "  >>  ", want = ">>"},
+Type = {
+	TEOF = 0,
+	TUNK = 1,
+	TWord = 2,
+	TIONumber = 3,
+	TNewLine = 4,
+	TAnd = 5,
+	TLess = 6,
+	TGreat = 7,
+	TDLess = 8,
+	TDGreat = 9,
+	TLessAnd = 10,
+	TGreatAnd = 11,
+	TLessGreat = 12,
+	TDLessDash = 13,
 }
 
-for _, tt in pairs(tests) do
+local tests = {
+	{input = "0", want = "0", type = Type.TIONumber},
+	{input = "1", want = "1", type = Type.TIONumber},
+	{input = "2", want = "2", type = Type.TIONumber},
+	{input = "3", want = "3", type = Type.TIONumber},
+	{input = "4", want = "4", type = Type.TIONumber},
+	{input = "5", want = "5", type = Type.TIONumber},
+	{input = "6", want = "6", type = Type.TIONumber},
+	{input = "7", want = "7", type = Type.TIONumber},
+	{input = "8", want = "8", type = Type.TIONumber},
+	{input = "9", want = "9", type = Type.TIONumber},
+	{input = "1234567890", want = "1234567890", type = Type.TIONumber},
+	{input = "&", want = "&", type = Type.TAnd},
+	{input = "<", want = "<", type = Type.TLess},
+	{input = ">", want = ">", type = Type.TGreat},
+	{input = "<&", want = "<&", type = Type.TLessAnd},
+	{input = ">&", want = ">&", type = Type.TGreatAnd},
+	{input = "<<", want = "<<", type = Type.TDLess},
+	{input = ">>", want = ">>", type = Type.TDGreat},
+
+	-- space at the left side
+	{input = " 0", want = "0", type = Type.TIONumber},
+	{input = " 1", want = "1", type = Type.TIONumber},
+	{input = " 2", want = "2", type = Type.TIONumber},
+	{input = " 3", want = "3", type = Type.TIONumber},
+	{input = " 4", want = "4", type = Type.TIONumber},
+	{input = " 5", want = "5", type = Type.TIONumber},
+	{input = " 6", want = "6", type = Type.TIONumber},
+	{input = " 7", want = "7", type = Type.TIONumber},
+	{input = " 8", want = "8", type = Type.TIONumber},
+	{input = " 9", want = "9", type = Type.TIONumber},
+	{input = " 1234567890", want = "1234567890", type = Type.TIONumber},
+	{input = " &", want = "&", type = Type.TAnd},
+	{input = " <", want = "<", type = Type.TLess},
+	{input = " >", want = ">", type = Type.TGreat},
+	{input = " <&", want = "<&", type = Type.TLessAnd},
+	{input = " >&", want = ">&", type = Type.TGreatAnd},
+	{input = " <<", want = "<<", type = Type.TDLess},
+	{input = " >>", want = ">>", type = Type.TDGreat},
+
+	-- space at the right side
+	{input = "1 ", want = "1", type = Type.TIONumber},
+	{input = "2 ", want = "2", type = Type.TIONumber},
+	{input = "3 ", want = "3", type = Type.TIONumber},
+	{input = "4 ", want = "4", type = Type.TIONumber},
+	{input = "5 ", want = "5", type = Type.TIONumber},
+	{input = "6 ", want = "6", type = Type.TIONumber},
+	{input = "7 ", want = "7", type = Type.TIONumber},
+	{input = " 8 ", want = "8", type = Type.TIONumber},
+	{input = " 9 ", want = "9", type = Type.TIONumber},
+	{input = "1234567890 ", want = "1234567890", type = Type.TIONumber},
+	{input = "& ", want = "&", type = Type.TAnd},
+	{input = "< ", want = "<", type = Type.TLess},
+	{input = "> ", want = ">", type = Type.TGreat},
+	{input = "<& ", want = "<&", type = Type.TLessAnd},
+	{input = ">& ", want = ">&", type = Type.TGreatAnd},
+	{input = "<< ", want = "<<", type = Type.TDLess},
+	{input = ">> ", want = ">>", type = Type.TDGreat},
+
+	-- space at in between
+	{input = "  0  ", want = "0", type = Type.TIONumber},
+	{input = "  1  ", want = "1", type = Type.TIONumber},
+	{input = "  2  ", want = "2", type = Type.TIONumber},
+	{input = "  3  ", want = "3", type = Type.TIONumber},
+	{input = "  4  ", want = "4", type = Type.TIONumber},
+	{input = "  5  ", want = "5", type = Type.TIONumber},
+	{input = "  6  ", want = "6", type = Type.TIONumber},
+	{input = "  7  ", want = "7", type = Type.TIONumber},
+	{input = "  8  ", want = "8", type = Type.TIONumber},
+	{input = "  9  ", want = "9", type = Type.TIONumber},
+	{input = "  1234567890  ", want = "1234567890", type = Type.TIONumber},
+	{input = "  &  ", want = "&", type = Type.TAnd},
+	{input = "  <  ", want = "<", type = Type.TLess},
+	{input = "  >  ", want = ">", type = Type.TGreat},
+	{input = "  <&  ", want = "<&", type = Type.TLessAnd},
+	{input = "  >&  ", want = ">&", type = Type.TGreatAnd},
+	{input = "  <<  ", want = "<<", type = Type.TDLess},
+	{input = "  >>  ", want = ">>", type = Type.TDGreat},
+
+	{input = "", want = "", type = Type.TEOF},
+	{input = "\n", want = "", type = Type.TEOF},
+	{input = "\t", want = "", type = Type.TEOF},
+	{input = "\n\t", want = "", type = Type.TEOF},
+	{input = "\t\n", want = "", type = Type.TEOF},
+	{input = "\t\n\t", want = "", type = Type.TEOF},
+	{input = "\n\t\n", want = "", type = Type.TEOF},
+
+	{input = "*", want = "*", type = Type.TUNK},
+	{input = "?", want = "?", type = Type.TUNK},
+	{input = "#", want = "#", type = Type.TUNK},
+}
+
+print 'lexer tests:'
+for k, tt in pairs(tests) do
 	lex.lex_make()
-	lex.lex_read(tt.input)
+	lex.lex_readfrom(tt.input)
 
 	local t = lex.lex_next()
 	local got = ffi.string(t.text)
 	
 	if got ~= tt.want then
-		print(string.format("got=%s, want=%s.", got, tt.want))
+		print(string.format("\ttoken.text test at k=%d: got=%s, \z
+			want=%s", k, got, tt.want))
+	end
+
+	if t.type ~= tt.type then
+		print(string.format("\ttoken.type test at k=%d: got=%s, \z
+			want=%s", k, t.type, tt.type))
 	end
 end
