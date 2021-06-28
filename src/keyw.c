4989ad061ebe6b2f96b8ea1961883434541d71f5
k <mol.tired@gmail.com>
2021-06-28 11:21:17 -0500

    keyw: add keyword recognizer

---
+++ src/keyw.c
@@ -0,0 +1,66 @@
+#include <stdbool.h>
+#include "lex.h"
+
+#define LENGTH 12
+
+static char *keywords[LENGTH] = {
+    "if",
+    "then",
+    "else",
+    "elif",
+    "fi",
+    "do",
+    "done",
+    "case",
+    "esac",
+    "until",
+    "while",
+    "for",
+};
+
+static int keywordtypes[LENGTH] = {
+    TIf,
+    TThen,
+    TElse,
+    TElif,
+    TFi,
+    TDo,
+    TDone,
+    TCase,
+    TEsac,
+    TUntil,
+    TWhile,
+    TFor,
+};
+
+TokenType keyw_gettype(const char *);
+static bool streq(const char *, const char *);
+
+// ---------------------------------------------------------------------------
+
+// keyw_getttype returns the TokenType of the keyword provided.  If the
+// provided keyword is, actually, not a keyword, keyw_gettype returns the type
+// TWord.
+TokenType keyw_gettype(const char *keyword)
+{
+    for (int i = 0; i < LENGTH; i++) {
+	if (streq(keyword, keywords[i])) {
+	    return keywordtypes[i];
+	}
+    }
+    return TWord;
+}
+
+// streq checks whether s1 and s2 are equal.
+static bool streq(const char *s1, const char *s2)
+{
+    while (*s1) {
+	s1++;
+	s2++;
+
+	if (*s1 != *s2) {
+	    return false;
+	}
+    }
+    return true;
+}
---
+++ src/keyw.h
@@ -0,0 +1,9 @@
+#ifndef KEYW_H
+#define kEYW_H
+
+#include <stdbool.h>
+#include "lex.h"
+
+TokenType keyw_gettype(const char *);
+
+#endif
